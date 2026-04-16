8c65d6f864d10b513e14ecf9f2266d5ed3971bc2
Khwaish Tiwari <khwaish04tiwari@gmail.com>
2026-04-17 01:40:52 +0530

    Added Spring Security and secured application configuration

--- src/main/java/com/khwaish/journalApp/JournalEntryController/UserController.java
+++ src/main/java/com/khwaish/journalApp/JournalEntryController/UserController.java
@@ -1,13 +1,14 @@
 package com.khwaish.journalApp.JournalEntryController;
 
 import com.khwaish.journalApp.entity.User;
+import com.khwaish.journalApp.repository.UserRepository;
 import com.khwaish.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -16,24 +17,24 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUsers(){
-        return userService.getAll();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping
-    public void createUser(@RequestBody User user) {
-        userService.saveEntry(user);
-    }
-
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName) {
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = userService.findByUserName(userName);
-        if (userInDb != null) {
-            userInDb.setUserName(user.getUserName());
-            userInDb.setPassword(user.getPassword());
-            userService.saveEntry(userInDb);
-        }
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+        userService.saveNewUser(userInDb);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUserById() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/khwaish/journalApp/service/UserService.java
+++ src/main/java/com/khwaish/journalApp/service/UserService.java
@@ -1,26 +1,33 @@
 package com.khwaish.journalApp.service;
 
-import com.khwaish.journalApp.entity.JournalEntry;
 import com.khwaish.journalApp.entity.User;
-import com.khwaish.journalApp.repository.JournalEntryRepository;
 import com.khwaish.journalApp.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 @Slf4j
 public class UserService {
-
     @Autowired
     private UserRepository userRepository;
 
-    public void saveEntry(User user) {
+    private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public void saveNewUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Arrays.asList("USER"));
+        userRepository.save(user);
+    }
+
+    public void saveUser(User user) {
         userRepository.save(user);
     }
 
